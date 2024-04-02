2f417acbb6630801d8e3b45174151aac76ca314b
John <johnnkonge2020@gmail.com>
2024-04-02 14:59:56 +0300

    updated lp

--- betting/src/components/landing.jsx
+++ betting/src/components/landing.jsx
@@ -3,11 +3,12 @@ import './landing.css'
 import phone from '../img/phone.png'
 import phone2 from '../img/phone2.png'
 import phone3 from '../img/phone3.png'
+import phone4 from'../img/phone4.png'
 import Footer from "./Footer";
 function LandingPage(){
     return(
         <div>
-            
+         <div>
             <div className="container-fluid">
                 
                 <div className="row1">
@@ -44,7 +45,7 @@ function LandingPage(){
                         <p> The Artificial Intelligence (AI) that underpins the Aviator Predictor app is designed to provide a reliable forecast of the plane's drop point with 99% accuracy. Utilizing the forecasts provided by the app, you can alter your life. https://aviatorpredict.com You need to execute simple steps for registration and app activation, then download the apk file for your mobile phone. Our Aviator Predictor Premium app supports mobile devices running on Android and iOS operating systems.
                         </p>
                         <br />
-                        <button className="btn">Download Hack</button>
+                        <button className="btn">Download apk</button>
                     </div>
                 </div>
             </div>
@@ -58,13 +59,33 @@ function LandingPage(){
                         <p> By using the forecast provided by our Aviator Predictor Hack, you can earn daily profits of upto 100% per day from the amount of your deposit. To avoid issues such as account suspension or payout hold, you should strictly adhere to the rules and regulations which we will explain during the regiatration process. We strongly recommend that you familiarize yourself with the documentation before using the app. For beginner, we recommend you start by playing the demo version of Aviator.
                         </p>
                         <br />
-                        <button className="btn">Download Hack</button>
+                        <button className="btn">Download hack</button>
                     </div>
                     <div className="column">
                     <img className="phone2" src={phone3} alt="phone" />
                     </div>
                 </div>
             </div>
+
+            <br />
+            <div className="container-fluid">
+                
+                <div className="row">
+                    <div className="column">
+                    <img className="phone2" src={phone4} alt="phone" />
+                    </div>
+                    <div className="column4">
+                        <h1 >Safe Usage Scheme</h1>
+                        <br />
+                        <p> You need to follow the rules and strictly adhere to the instructions for using the application. For these purposes, we limit the operations for the Predictor Aviator app for new users. More detailed information about the scheme to adhere to is available on our website.
+                        </p>
+                        <br />
+                        <button className="btn">Download app</button>
+                    </div>
+                </div>
+            </div>
+         </div>
+         
             <div className="footer">
                 <Footer/>
             </div>
